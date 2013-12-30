4a1eb1c74c744c29f879fd0d2171eeb47a77fc69
Jing Xiao <notajingoist@gmail.com>
2013-12-30 02:41:03 -0500

    new bookmarrrkss plus thumbnail hover thinggg

--- js/src/scripts.js
+++ js/src/scripts.js
@@ -2,15 +2,48 @@ var SITE = {
 	init: function() {
 		this.$document = $(window.document);
 		this.$body = $('body');
+		this.$tns = $('.animate');
 
 		this.bindEvents();
+
+		this.tnHovered = false;
 	},
 
 	bindEvents: function() {
-		//this.$body.on('click', this.blah.bind(this));
+		this.$tns.on('mouseenter', this.animateEnter.bind(this));
+		this.$tns.on('mouseleave', this.animateLeave.bind(this));
 	},
 
-	blah: function() {
+	animateEnter: function(e) {
+		var $img = $(e.currentTarget);
+
+		if (!this.tnHovered) {
+			var currSrc = $img.attr('src');
+			var newSrc = currSrc
+				.slice(0, currSrc.length - 4)
+				.concat('-2.jpg');
+			$img.attr('src', newSrc);
+
+			this.tnHovered = true;
+		}
+
+		e.preventDefault();
+	},
+
+	animateLeave: function(e) {
+		var $img = $(e.currentTarget);
+
+		if (this.tnHovered) {
+			var currSrc = $img.attr('src');
+			var newSrc = currSrc
+				.slice(0, currSrc.length - 6)
+				.concat('.jpg');
+			$img.attr('src', newSrc);
+		
+			this.tnHovered = false;
+		}
+
+		e.preventDefault();	
 	}
 }
 
